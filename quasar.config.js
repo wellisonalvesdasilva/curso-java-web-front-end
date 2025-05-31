e4d9d082ba9f26989739ca5af9bd66a3d08bef0a
Wellison Alves da Silva <990797@bancoamazoniacombr.onmicrosoft.com>
2025-05-31 11:42:31 -0300

    fix

--- quasar.config.js
+++ quasar.config.js
@@ -8,7 +8,7 @@
 // Configuration for your app
 // https://v2.quasar.dev/quasar-cli-vite/quasar-config-js
 
-const ESLintPlugin = require('eslint-webpack-plugin')
+import ESLintPlugin from 'eslint-webpack-plugin'
 const { configure } = require('quasar/wrappers')
 require('dotenv').config()
 
