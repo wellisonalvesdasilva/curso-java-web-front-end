a6b23e4ca58ad720c095bd423068ae5dca7a512e
Wellison Alves da Silva <990797@bancoamazoniacombr.onmicrosoft.com>
2025-05-31 12:04:35 -0300

    fix

--- src/boot/axios.js
+++ src/boot/axios.js
@@ -2,7 +2,7 @@ import { boot } from 'quasar/wrappers'
 import axios from 'axios'
 import validators from '../validators/validator-set'
 
-const api = axios.create({ baseURL: 'http://localhost:8080/v1/api' })
+const api = axios.create({ baseURL: 'https://mercado-instrumental-api-production.up.railway.app/v1/api' })
 
 export default boot(({ app }) => {
   app.config.globalProperties.$axios = axios
